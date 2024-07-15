c564d6946d3613f4d0ed7dcd8cf00c9cc03ee7c0
Ahmed Osman <ahmedibrahimxzxzxz@gmail.com>
2024-07-16 01:55:35 +0300

    Restarting the project from scratch

--- js/main.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const menuIcon = document.getElementById("menu-icon");
-    const navbar = document.getElementById("navbar");
-
-    menuIcon.addEventListener("click", function() {
-        navbar.classList.toggle("menu-open");
-    });
-});
