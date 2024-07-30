21bd58f5550daf5294c47ab797a0384fb1d6e710
Ahmed Osman <ahmedibrahimxzxzxz@gmail.com>
2024-07-30 14:20:24 +0300

    Refactored the code to get the navbar animation to work on expanding and collapsing the navbar widget

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,19 @@ const navbar = document.querySelector(".navbar");
 const navbarBackdrop = document.querySelector(".backdrop");
 const hamburgerIcon = document.querySelector(".hamburger__icon");
 
+
+
 // Toggle the navbar show/hide
 function toggleNavbar() {
-    // Showing Navbar
-    navbar.classList.toggle("navbar__show");
+    // Show/Hide navbar
+    navbar.classList.toggle("expand__navbar");
+    if (navbar.classList.contains("expand__navbar")) {
+        // Remove the collapse class if the navbar was expanded
+        navbar.classList.remove("collapse__navbar");
+    } else {
+        // Add the collapse class if the navbar was collapsed
+        navbar.classList.add("collapse__navbar");
+    }
     // Making a blur background for the navigation on small screens
     navbarBackdrop.classList.toggle("blur");
     // Animation on the hamburger icon when expanding the navbar
@@ -15,12 +24,15 @@ function toggleNavbar() {
 
 // Hide the navbar
 function hideNavbar() {
-    const navbarShown = navbar.classList.contains("navbar__show");
+    const navbarShown = navbar.classList.contains("expand__navbar");
     const backdropShown = navbarBackdrop.classList.contains("blur");
     const menuOpened = hamburgerIcon.classList.contains("hamburger__icon__active");
 
     // Hide Navbar Widget
-    if (navbarShown) navbar.classList.remove("navbar__show");
+    if (navbarShown)  {
+        navbar.classList.remove("expand__navbar");
+        navbar.classList.add("collapse__navbar");
+    }
     // Hide Backdrop
     if (backdropShown) navbarBackdrop.classList.remove("blur");;
     // Close Menu Icon
